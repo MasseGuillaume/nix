0c56952bc1a94ba4ad12995c4528f0d9349d3630
Guillaume Massé <masgui@gmail.com>
2021-04-13 22:57:20 -0400

    Move/Focus displays

--- home.nix
+++ home.nix
@@ -88,103 +88,88 @@ in
     sway = {
       enable = true;
       systemdIntegration = false;
-      config = {       
-        focus.mouseWarping = false;
-        window.border = 0;
-        modifier = "Mod1";
-        bars = [];
-        modes = {};
-        keybindings =
-          let
-            mod = "Mod1";
-          in
-            {
-              "${mod}+Return" = "exec kitty";
-              "${mod}+Shift+q" = "kill";
-              "${mod}+d" = "exec dmenu_run";
+      config =
+        let
+          mod = "Mod1";
+          leftOutput = "DVI-D-1";
+          centerOutput = "HDMI-A-1";
+          rightOutput = "DP-1";
+        in {
+          focus.mouseWarping = false;
+          window.border = 0;
+          modifier = "Mod1";
+          bars = [];
+          modes = {};
+          keybindings = {
+            "${mod}+Return"  = "exec kitty";
+            "${mod}+k"       = "kill";
+            "${mod}+d"       = "exec dmenu_run";
+            "${mod}+f"       = "fullscreen toggle";
+            "${mod}+Shift+q" = "exec swaynag -t warning -m 'Kill Saw?' -b 'Yes' 'swaymsg exit'";
+            "${mod}+F1"      = "exec ${screenshot}";
+            "${mod}+space"   = "floating toggle";
 
-              "${mod}+h" = "focus left";
-              "${mod}+s" = "focus right";
-              "${mod}+n" = "focus down";
-              "${mod}+t" = "focus up";
-              
-              "${mod}+Shift+h" = "move left";
-              "${mod}+Shift+s" = "move right";
-              "${mod}+Shift+n" = "move down";
-              "${mod}+Shift+t" = "move up";
 
-              "${mod}+Shift+g" = "resize shrink width 25 px";
-              "${mod}+Shift+c" = "resize grow height 25 px";
-              "${mod}+Shift+r" = "resize shrink height 25 px";
-              "${mod}+Shift+l" = "resize grow width 25 px";
-
-              "${mod}+b" = "splith";
-              "${mod}+v" = "splitv";
-              "${mod}+f" = "fullscreen toggle";
-              "${mod}+a" = "focus parent";
-
-              # "${mod}+s" = "layout stacking";
-              "${mod}+w" = "layout tabbed";
-              "${mod}+e" = "layout toggle split";
-
-              "${mod}+Shift+space" = "floating toggle";
-              "${mod}+space" = "focus mode_toggle";
-
-              "${mod}+1" = "workspace number 1";
-              "${mod}+2" = "workspace number 2";
-              "${mod}+3" = "workspace number 3";
-              "${mod}+4" = "workspace number 4";
-              "${mod}+5" = "workspace number 5";
-              "${mod}+6" = "workspace number 6";
-              "${mod}+7" = "workspace number 7";
-              "${mod}+8" = "workspace number 8";
-              "${mod}+9" = "workspace number 9";
-
-              "${mod}+Shift+1" = "move container to workspace number 1";
-              "${mod}+Shift+2" = "move container to workspace number 2";
-              "${mod}+Shift+3" = "move container to workspace number 3";
-              "${mod}+Shift+4" = "move container to workspace number 4";
-              "${mod}+Shift+5" = "move container to workspace number 5";
-              "${mod}+Shift+6" = "move container to workspace number 6";
-              "${mod}+Shift+7" = "move container to workspace number 7";
-              "${mod}+Shift+8" = "move container to workspace number 8";
-              "${mod}+Shift+9" = "move container to workspace number 9";
-
-              "${mod}+Shift+minus" = "move scratchpad";
-              "${mod}+minus" = "scratchpad show";
-
-              "${mod}+Shift+u" = "reload";
-              "${mod}+Shift+e" = "exec swaynag -t warning -m 'Kill Saw?' -b 'Yes' 'swaymsg exit'";
-
-              "${mod}+r" = "mode resize";
-
-              "${mod}+F1" = "exec ${screenshot}";
-            };
-
-        # DVI-D-0 | HDMI-A-0 | DisplayPort-0
-        output = {
-          "*" = {
-            transform = "90";
-            background = "#000000 solid_color";
-          };
-          "DVI-D-1" = {
-            pos = "0 0";
+            "${mod}+h" = "focus left";
+            "${mod}+s" = "focus right";
+            "${mod}+n" = "focus down";
+            "${mod}+t" = "focus up";
             
+            "${mod}+Shift+h" = "move left";
+            "${mod}+Shift+s" = "move right";
+            "${mod}+Shift+n" = "move down";
+            "${mod}+Shift+t" = "move up";
+
+            "${mod}+Shift+g" = "resize shrink width 25 px";
+            "${mod}+Shift+c" = "resize grow height 25 px";
+            "${mod}+Shift+r" = "resize shrink height 25 px";
+            "${mod}+Shift+l" = "resize grow width 25 px";
+
+            "${mod}+o" = "focus output ${leftOutput}";
+            "${mod}+e" = "focus output ${centerOutput}";
+            "${mod}+u" = "focus output ${rightOutput}";
+
+            "${mod}+Shift+o" = "move container to output ${leftOutput}";
+            "${mod}+Shift+e" = "move container to output ${centerOutput}";
+            "${mod}+Shift+u" = "move container to output ${rightOutput}";
+            
+            "${mod}+1" = "workspace number 1";
+            "${mod}+2" = "workspace number 2";
+            "${mod}+3" = "workspace number 3";
+            "${mod}+4" = "workspace number 4";
+            "${mod}+5" = "workspace number 5";
+            "${mod}+6" = "workspace number 6";
+            "${mod}+7" = "workspace number 7";
+            "${mod}+8" = "workspace number 8";
+            "${mod}+9" = "workspace number 9";
+            
+            "${mod}+Shift+1" = "move container to workspace number 1";
+            "${mod}+Shift+2" = "move container to workspace number 2";
+            "${mod}+Shift+3" = "move container to workspace number 3";
+            "${mod}+Shift+4" = "move container to workspace number 4";
+            "${mod}+Shift+5" = "move container to workspace number 5";
+            "${mod}+Shift+6" = "move container to workspace number 6";
+            "${mod}+Shift+7" = "move container to workspace number 7";
+            "${mod}+Shift+8" = "move container to workspace number 8";
+            "${mod}+Shift+9" = "move container to workspace number 9";
           };
-          "HDMI-A-1" = {
-            pos = "1080 0";
+
+          output = {
+            "*" = {
+              transform = "90";
+              background = "#000000 solid_color";
+            };
+            "${leftOutput}"   = { pos = "0 0";    };
+            "${centerOutput}" = { pos = "1080 0"; };
+            "${rightOutput}"  = { pos = "2160 0"; };
           };
-          "DP-1" = {
-            pos = "2160 0";
+          input = {
+            "type:keyboard" = {
+              repeat_delay = "200";
+              repeat_rate = "50";
+            };
           };
         };
-        input = {
-          "type:keyboard" = {
-            repeat_delay = "200";
-            repeat_rate = "50";
-          };
-        };
-      };
     };
   };
 
