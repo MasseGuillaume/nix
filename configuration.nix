7deb98b70d4fc3ac8f511dafafc86a43e69ee681
Guillaume Massé <masgui@gmail.com>
2021-04-22 19:38:27 -0400

    Revert to xmonad

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,10 @@
     ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    # extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    # kernelModules = [ "v4l2loopback" ];
+    # kernelPackages = pkgs.linuxPackages_latest;
+
     # kernelParams = [ "intel_pstate=active" ];
     blacklistedKernelModules = [ "nouveau" ];
     loader = {
@@ -22,14 +25,125 @@
 
   networking = {
     hostName = "bob";
-    nameservers = [ "8.8.8.8" ];
+    # nameservers = [ "8.8.8.8" ];
   };
 
   time.timeZone = "America/Montreal";
 
   services = {
-    xserver.enable = true;
+    bloop = {
+      install = true;
+    };
     udev.packages = [ pkgs.yubikey-personalization ];
+    xserver = {
+      videoDrivers = ["amdgpu"];
+      enable = true;
+  
+      windowManager = {        
+        xmonad.enable = true;
+        xmonad.enableContribAndExtras = true;
+      };
+
+      desktopManager = {
+        xterm.enable = true;
+      };
+  
+      displayManager = {
+        defaultSession = "none+xmonad";
+
+        lightdm.enable = true;
+
+        autoLogin = {
+          enable = true;
+          user = "gui";
+        };
+          
+        sessionCommands = ''
+          ${pkgs.xlibs.xrdb}/bin/xrdb -all ~/.Xresources
+          ${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr
+          ${pkgs.xlibs.xset}/bin/xset r rate 200 50
+          ${pkgs.hsetroot}/bin/hsetroot -solid '#002b36'
+          #   DVI-D-0     |  HDMI-A-0   |    DisplayPort-0
+          ${pkgs.xlibs.xrandr}/bin/xrandr --output DVI-D-0        --rotate right --pos    0x0
+          ${pkgs.xlibs.xrandr}/bin/xrandr --output HDMI-A-0       --rotate right --pos 1080x0
+          ${pkgs.xlibs.xrandr}/bin/xrandr --output DisplayPort-0  --rotate right --pos 2160x0
+          
+          xrdb "${pkgs.writeText  "xrdb.conf" ''
+              URxvt.font:                 xft:Dejavu Sans Mono for Powerline:size=11
+              XTerm*faceName:             xft:Dejavu Sans Mono for Powerline:size=11
+              XTerm*utf8:                 2
+              # URxvt.iconFile:             /usr/share/icons/elementary/apps/24/terminal.svg
+              URxvt.letterSpace:          0
+              #define S_base03        #002b36
+              #define S_base02        #073642
+              #define S_base01        #586e75
+              #define S_base00        #657b83
+              #define S_base0         #839496
+              #define S_base1         #93a1a1
+              #define S_base2         #eee8d5
+              #define S_base3         #fdf6e3
+              *background:            S_base03
+              *foreground:            S_base0
+              *fadeColor:             S_base03
+              *cursorColor:           S_base1
+              *pointerColorBackground:S_base01
+              *pointerColorForeground:S_base1
+              !! black dark/light
+              *color0:                S_base02
+              *color8:                S_base03
+              
+              !! red dark/light
+              *color1:                S_red
+              *color9:                S_orange
+              
+              !! green dark/light
+              *color2:                S_green
+              *color10:               S_base01
+              
+              !! yellow dark/light
+              *color3:                S_yellow
+              *color11:               S_base00
+              
+              !! blue dark/light
+              *color4:                S_blue
+              *color12:               S_base0
+              
+              !! magenta dark/light
+              *color5:                S_magenta
+              *color13:               S_violet
+              
+              !! cyan dark/light
+              *color6:                S_cyan
+              *color14:               S_base1
+              
+              !! white dark/light
+              *color7:                S_base2
+              *color15:               S_base3
+              URxvt*saveLines:            32767
+              XTerm*saveLines:            32767
+              URxvt.colorUL:              #AED210
+              URxvt.perl-ext:             default,url-select
+              URxvt.keysym.M-u:           perl:url-select:select_next
+              URxvt.url-select.launcher:  /usr/bin/firefox -new-tab
+              URxvt.url-select.underline: true
+              Xft*dpi:                    96
+              Xft*antialias:              true
+              Xft*hinting:                full
+              URxvt.scrollBar:            false
+              URxvt*scrollTtyKeypress:    true
+              URxvt*scrollTtyOutput:      false
+              URxvt*scrollWithBuffer:     false
+              URxvt*scrollstyle:          plain
+              URxvt*secondaryScroll:      true
+              Xft.autohint: 0
+              Xft.lcdfilter:  lcddefault
+              Xft.hintstyle:  hintfull
+              Xft.hinting: 1
+              Xft.antialias: 1 
+          ''}"
+        '';
+      };
+    };
   };
 
   hardware = {
@@ -75,8 +189,6 @@
   };
 
   programs = {
-    sway.enable = true;
-
     zsh = {
       enable = true;
       ohMyZsh = {
@@ -115,7 +227,15 @@
     autoUpgrade = {
       enable = true;
       allowReboot = false;
-      # channel = https://nixos.org/channels/nixos-20.09;
+      channel = https://nixos.org/channels/nixos-20.09;
+
+      # sudo nix-channel --list
+      # nixos https://channels.nixos.org/nixos-unstable
+      # nixpkgs https://channels.nixos.org/nixpkgs-unstable
+      
+      
+      # sudo nix-channel --add https://channels.nixos.org/nixpkgs-unstable nixos
+      # sudo nix-channel --add https://channels.nixos.org/nixos-20.09 nixos
     };
     stateVersion = "20.03";
   };
--- home.nix
+++ home.nix
@@ -38,22 +38,12 @@ in
     wget
     yarn
     zoom-us
-    wl-clipboard
   ];
 
   programs = {
     vscode.enable = true;
     jq.enable = true;
     htop.enable = true;
-
-    alacritty = {
-      enable = true;
-    };
-
-    password-store = {
-      enable = true;
-      package = pkgs.pass-wayland;
-    };
 
     chromium = {
       enable = true;
@@ -76,108 +66,11 @@ in
     };
   };
 
-  wayland.windowManager = rec {
-    sway = {
-      enable = true;
-      systemdIntegration = false;
-      config =
-        let
-          mod = "Mod1";
-          leftOutput = "DVI-D-1";
-          centerOutput = "HDMI-A-1";
-          rightOutput = "DP-1";
-        in {
-          focus.mouseWarping = false;
-          window.border = 0;
-          modifier = "Mod1";
-          bars = [];
-          modes = {};
-          keybindings = {
-            "${mod}+Return"        = "exec alacritty";
-            "${mod}+Shift+Return"  = "exec alacritty";
-            "${mod}+k"             = "kill";
-            "${mod}+d"             = "exec dmenu_run";
-            "${mod}+f"             = "fullscreen toggle";
-            "${mod}+Shift+q"       = "exec swaynag -t warning -m 'Kill Saw?' -b 'Yes' 'swaymsg exit'";
-            "${mod}+space"         = "floating toggle";
-            "${mod}+F1"            = "input type:keyboard xkb_layout us";
-            "${mod}+F2"            = "input type:keyboard xkb_layout ca";
-            "${mod}+F3"            = "input type:keyboard xkb_layout ir";
-            "${mod}+F5"            = "exec ${screenshot}";
-
-            "${mod}+h" = "focus left";
-            "${mod}+s" = "focus right";
-            "${mod}+n" = "focus down";
-            "${mod}+t" = "focus up";
-            
-            "${mod}+Shift+h" = "move left";
-            "${mod}+Shift+s" = "move right";
-            "${mod}+Shift+n" = "move down";
-            "${mod}+Shift+t" = "move up";
-
-            "${mod}+Shift+g" = "resize shrink width 25 px";
-            "${mod}+Shift+c" = "resize grow height 25 px";
-            "${mod}+Shift+r" = "resize shrink height 25 px";
-            "${mod}+Shift+l" = "resize grow width 25 px";
-
-            "${mod}+o" = "focus output ${leftOutput}";
-            "${mod}+e" = "focus output ${centerOutput}";
-            "${mod}+u" = "focus output ${rightOutput}";
-
-            "${mod}+Shift+o" = "move container to output ${leftOutput}";
-            "${mod}+Shift+e" = "move container to output ${centerOutput}";
-            "${mod}+Shift+u" = "move container to output ${rightOutput}";
-
-            "${mod}+m"       = "output ${leftOutput} transform 0";
-            "${mod}+Shift+m" = "output ${leftOutput} transform 90";
-
-            "${mod}+1" = "workspace number 1";
-            "${mod}+2" = "workspace number 2";
-            "${mod}+3" = "workspace number 3";
-            "${mod}+4" = "workspace number 4";
-            "${mod}+5" = "workspace number 5";
-            "${mod}+6" = "workspace number 6";
-            "${mod}+7" = "workspace number 7";
-            "${mod}+8" = "workspace number 8";
-            "${mod}+9" = "workspace number 9";
-            
-            "${mod}+Shift+1" = "move container to workspace number 1";
-            "${mod}+Shift+2" = "move container to workspace number 2";
-            "${mod}+Shift+3" = "move container to workspace number 3";
-            "${mod}+Shift+4" = "move container to workspace number 4";
-            "${mod}+Shift+5" = "move container to workspace number 5";
-            "${mod}+Shift+6" = "move container to workspace number 6";
-            "${mod}+Shift+7" = "move container to workspace number 7";
-            "${mod}+Shift+8" = "move container to workspace number 8";
-            "${mod}+Shift+9" = "move container to workspace number 9";
-          };
-
-          output = {
-            "*" = {
-              transform = "90";
-              background = "#000000 solid_color";
-            };
-            "${leftOutput}"   = { pos = "0 0";    };
-            "${centerOutput}" = { pos = "1080 0"; };
-            "${rightOutput}"  = { pos = "2160 0"; };
-          };
-          input = {
-            "type:keyboard" = {
-              repeat_delay = "200";
-              repeat_rate = "50";
-            };
-          };
-        };
-    };
-  };
-
-  news.display = "silent";
-
   nixpkgs.config = {
     allowUnfree = true;
     packageOverrides = pkgs : rec {
+      
       pulumi = pkgs.callPackage /home/gui/foos/nixpkgs/pkgs/tools/admin/pulumi/default.nix { };
-      vscode = pkgs.callPackage /home/gui/foos/nixpkgs/pkgs/applications/editors/vscode/vscode.nix { };
       postgresql = pkgs.postgresql_12;
       yarn = pkgs.yarn.override { 
         nodejs = pkgs.nodejs-12_x;
@@ -188,6 +81,14 @@ in
       sbt = pkgs.callPackage /home/gui/foos/nixpkgs/pkgs/development/tools/build-managers/sbt/default.nix {
         jre = pkgs.graalvm11-ce; 
       };
+      
+      vscode = pkgs.callPackage /home/gui/foos/nixpkgs/pkgs/applications/editors/vscode/vscode.nix { };
+      zoom-us = (pkgs.callPackage /home/gui/foos/nixpkgs/pkgs/applications/networking/instant-messengers/zoom-us {
+        util-linux = pkgs.callPackage /home/gui/foos/nixpkgs/pkgs/os-specific/linux/util-linux {};
+      }).overrideAttrs (old: {
+      postFixup = old.postFixup + ''
+        wrapProgram $out/bin/zoom-us --unset XDG_SESSION_TYPE
+      '';});
     };
   };
 }
